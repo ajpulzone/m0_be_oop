cc42937c7d41fc04ca51b9039ad2faadf241415b
Annie Pulzone <ajpulzone@gmail.com>
2022-07-18 22:10:27 -0700

    Initial commit

--- oop.rb
+++ oop.rb
@@ -1,13 +1,42 @@
 # Write a class called Unicorn
+class Unicorn
+  def say(color, horn_length, height)
+    @color = "Silver"
+    @horn_length = horn_length
+    @height = 15
+    @horn_color = horn_color
+  end
+end
+
+#
+# class EscapeCar
+#   def initialize(color, interior)
+#     @color = color
+#     @interior = interior
+#     @wheels = 4
+#     @doors = 4
+#   end
+# end
+#
+# car1 = EscapeCar.new("blue", "cloth")
+# p car1
+#
+# car2 = EscapeCar.new("blue", "cloth")
+# p car2
+#
+# car3 = EscapeCar.new("pink", "pleather")
+# p car3
 # it should have a dynamic name attribute
 # it should have a color attribute, that is silver by default
-# it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+# it should have a method called "say" that returns whatever
+# string is passed in, with "*~*" at the beginning and end of the string
 
 
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
-#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
+#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if
+# info is passed in initially
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
@@ -25,6 +54,9 @@
 #  it should have a dynamic disposition attribute (string)
 #  it should have an age attribute that defaults to 0
 #  it should have a celebrate_birthday method. When called, the age increases by 1
-#  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
-#  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
-#  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+#  it should have an is_adult attribute (boolean) that is false by default. once a
+# Hobbit is 33, it should be an adult
+#  it should have an is_old attribute that defaults to false. once a Hobbit is 101,
+# it is old.
+#  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not,
+# false.
